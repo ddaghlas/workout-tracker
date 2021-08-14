a4126b5406045baaa453b3a48a8f58f733f3576a
Diana Daghlas <dianadaghlas@yahoo.com>
2021-08-14 16:04:43 -0400

    Updated routes and model name

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,53 +1,86 @@
 // Import express router
 const router = require("express").Router();
+const Workout = require("../models/Workout.js");
 
 // Import workout model
-const db = require("../models/exercise");
+// const db = require("../models/exercise");
 
-// GET Request for getting all workouts
-// Add total duration to both get routes
-router.get("/api/workouts", (req, res) => {
-	db.find()
-		.then((dbData) => {
-			res.json(dbData);
+// Route to get workouts
+router.get("/workouts", (req, res) => {
+	// Get workout data, aggregating exercise duration into a field named totalDuration
+	  Workout.aggregate([
+		{
+		  '$sort': {
+			'day': 1
+		  }
+		}, {
+		  '$addFields': {
+			'totalDuration': {
+			  '$sum': '$exercises.duration'
+			}
+		  }
+		}
+	  ])
+		.then((dbWorkout) => {
+		  res.status(200).json(dbWorkout);
 		})
 		.catch((err) => {
-			res.json(err);
+		  res.status(400).json(err);
 		});
-});
-
-// GET request
-router.get("/api/workouts/range", (req, res) => {
-	db.find()
-		.then((dbData) => {
-			res.json(dbData);
+	});
+	
+	// Route to add exercises
+	router.put("/workouts/:id", (req, res) => {
+	  Workout.findByIdAndUpdate(req.params.id, {
+		$push: { exercises: req.body }
+	  })
+		.then((dbExercise) => {
+		  res.status(200).json(dbExercise);
 		})
 		.catch((err) => {
-			res.json(err);
+		  res.status(400).json(err);
 		});
-});
-
-// POST workout
-router.post("/api/workouts", ({ body }, res) => {
-	db.create(body)
-		.then((dbData) => {
-			res.json(dbData);
+	});
+	
+	// Route to create a new workout
+	router.post("/workouts", (req, res) => {
+	  Workout.create(req.body)
+		.then((workout) => {
+		  res.status(200).json(workout);
 		})
 		.catch((err) => {
-			res.json(err);
+		  res.status(400).json(err);
 		});
-});
-
-// PUT/Update workout
-router.put("/api/workouts/:id", ({ body, params }, res) => {
-	db.findByIdAndUpdate(params.id, { $push: { exercises: body } })
-		.then((dbData) => {
-			res.json(dbData);
+	});
+	
+	// Route to return a range of workouts
+	// Exercise duration is aggregated to totalDuration
+	router.get("/workouts/range", (req, res) => {
+	  Workout.aggregate([
+		{
+		  '$sort': {
+			'day': -1
+		  }
+		}, {
+		  '$limit': 7
+		}, {
+		  '$addFields': {
+			'totalDuration': {
+			  '$sum': '$exercises.duration'
+			}
+		  }
+		},{
+		  '$sort': {
+			'day': 1
+		  }
+		}
+	  ])
+		.then((dbWorkout) => {
+		  res.status(200).json(dbWorkout);
 		})
 		.catch((err) => {
-			res.json(err);
+		  res.status(400).json(err);
 		});
-});
-
-// Export API routes
-module.exports = router;
+	});
+	
+	module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -19,5 +19,9 @@ router.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 // Export HTML routes
 module.exports = router;
